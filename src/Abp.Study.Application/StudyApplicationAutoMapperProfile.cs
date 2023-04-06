92e3f3146e1a5c581158ab1ec7a7adfb5a532fe5
瑞曾 吴 <wrhming@hotmail.com>
2023-04-06 09:19:49 +0800

    1

--- src/Abp.Study.Application/StudyApplicationAutoMapperProfile.cs
+++ src/Abp.Study.Application/StudyApplicationAutoMapperProfile.cs
@@ -9,5 +9,6 @@ public class StudyApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+        //映射相关数据
     }
 }
